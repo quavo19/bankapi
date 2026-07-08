674bf96546bd3301ec6fa369d2835cbd65338cd4
Donald Akite <donaldakite27@gmail.com>
2026-07-08 16:06:44 +0000

    Refactor database connection setup in tests to use environment variable for DB_SOURCE, with a fallback to a default value.

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,13 +10,18 @@ import (
 )
 
 const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:07May2002@@localhost:5433/simple_bank?sslmode=disable"
+	dbDriver        = "postgres"
+	defaultDBSource = "postgresql://root:07May2002@@localhost:5433/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 
 func TestMain(m *testing.M) {
+	dbSource := os.Getenv("DB_SOURCE")
+	if dbSource == "" {
+		dbSource = defaultDBSource
+	}
+
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
