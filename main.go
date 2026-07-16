c8d31457562ff0e3ae60f81b5f94dad66a33762d
Donald Akite <donaldakite27@gmail.com>
2026-07-16 23:25:03 +0000

    Refactor main function to load database configuration from a config file, replacing hardcoded values with dynamic settings.

--- main.go
+++ main.go
@@ -3,25 +3,27 @@ package main
 import (
 	"log"
 	"database/sql"
+	"github.com/quavo19/bankapi/util"
 	db "github.com/quavo19/bankapi/db/sqlc"
 	"github.com/quavo19/bankapi/api"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver        = "postgres"
-	defaultDBSource = "postgresql://root:07May2002@@localhost:5433/simple_bank?sslmode=disable"
-	serverAddress   = "0.0.0.0:8080"
-)
 
 func main() {
-	conn, err := sql.Open(dbDriver, defaultDBSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
+
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
